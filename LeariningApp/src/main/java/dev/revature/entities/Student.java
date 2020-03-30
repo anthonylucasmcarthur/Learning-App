1ec551d08c56e1717d1df7fd3f2a9ba2329bd3a8
sscollier17 <60713097+sscollier17@users.noreply.github.com>
2020-03-30 12:00:57 -0400

    added entities

---
+++ LeariningApp/src/main/java/dev/revature/entities/Student.java
@@ -0,0 +1,123 @@
+package dev.revature.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Student {
+
+	@GeneratedValue
+	@Id
+	@Column(name = "s_id")
+	private int id;
+	
+	@Column(name = "username")
+	private String username;
+	
+	@Column(name = "firstname")
+	private String firstname;
+	
+	
+	@Column(name = "lastname")
+	private String lastname;
+	
+	
+	@Column(name = "password")
+	private String password;
+
+
+	@ManyToOne
+	@JoinColumn(name = "t_id")
+	
+	private int tid;
+
+	public Student(int id, String username, String firstname, String lastname, String password, int tid) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.password = password;
+		this.tid = tid;
+	}
+
+
+	public Student() {
+		super();
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+
+	public String getLastname() {
+		return lastname;
+	}
+
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setTid(int tid) {
+		this.tid = tid;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", username=" + username + ", firstname=" + firstname + ", lastname=" + lastname
+				+ ", password=" + password + ", getId()=" + getId() + ", getUsername()=" + getUsername()
+				+ ", getFirstname()=" + getFirstname() + ", getLastname()=" + getLastname() + ", getPassword()="
+				+ getPassword() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "tid = tid "]";"
+	}
+	
+	
+	
+}
