7acc06a331c564ded70b65092ff0de9db65cea72
anthonylucasmcarthur <60712819+anthonylucasmcarthur@users.noreply.github.com>
2020-03-30 12:26:51 -0400

    testing repositories

--- LeariningApp/src/main/java/dev/learning/repositories/AssignmentRepo.java
+++ LeariningApp/src/main/java/dev/learning/repositories/AssignmentRepo.java
@@ -13,10 +13,11 @@ import dev.learning.entities.Teacher;
 public interface AssignmentRepo extends CrudRepository<Assignment, Integer>{
 	
 	public Assignment findById(int id);
-	public Assignment createAssignment(String description, String duedate, double weight);
-	public Set<Assignment> viewAssignments(Teacher teacher);
-	public Set<Assignment> viewAssignments(Student student);
-	public Assignment gradeAssignment(double grade, String comment);
+	public Assignment createAssignment(Assignment assignment);
+//	public Set<Assignment> viewAssignments(Teacher teacher);
+//	public Set<Assignment> viewAssignments(Student student);
+	public Assignment findAssignmentByStudent(Student student);
+	public Assignment gradeAssignment(long grade, String comment);
 	public boolean deleteAssignment(Assignment assignment);
 
 }
