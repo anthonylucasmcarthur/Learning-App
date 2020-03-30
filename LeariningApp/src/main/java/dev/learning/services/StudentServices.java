760b569f075f46038723a9fa5ab85fb311cf0fd7
sscollier17 <60713097+sscollier17@users.noreply.github.com>
2020-03-30 13:17:21 -0400

    services

---
+++ LeariningApp/src/main/java/dev/learning/services/StudentServices.java
@@ -0,0 +1,15 @@
+package dev.learning.services;
+
+import java.util.Set;
+
+import dev.learning.entities.Student;
+import dev.learning.entities.Teacher;
+
+public interface StudentServices {
+
+	
+	public Student login(String username, String password);
+	public Set<Student> viewStudents(Teacher teacher);
+	
+	
+}
