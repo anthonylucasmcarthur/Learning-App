1f798cb917ce9b8b3137240423c6b0101feb4651
anthonylucasmcarthur <60712819+anthonylucasmcarthur@users.noreply.github.com>
2020-03-30 10:23:45 -0400

    Add files via upload

---
+++ LeariningApp/src/main/java/dev/learning/LeariningAppApplication.java
@@ -0,0 +1,13 @@
+package dev.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeariningAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeariningAppApplication.class, args);
+	}
+
+}
