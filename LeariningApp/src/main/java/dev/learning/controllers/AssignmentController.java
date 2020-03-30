57ccfc792c4ebb8a42aff85ac9c52e3d25403aae
anthonylucasmcarthur <60712819+anthonylucasmcarthur@users.noreply.github.com>
2020-03-30 13:51:39 -0400

    controller methods

---
+++ LeariningApp/src/main/java/dev/learning/controllers/AssignmentController.java
@@ -0,0 +1,43 @@
+package dev.learning.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.learning.entities.Assignment;
+import dev.learning.services.ServicesImplementation;
+
+@Component
+@RestController
+@RequestMapping("/assignment")
+@CrossOrigin
+public class AssignmentController {
+
+	@Autowired
+	private ServicesImplementation si;
+	
+	@PostMapping(value = "/create")
+	public Assignment addAssignment(@RequestBody(required = true) Assignment a) {
+		String description = a.getDescription();
+		return si.AssignAssignment(a);
+	}
+
+	@PutMapping(value = "/grade/{id}", produces = "application/json")
+	public Assignment gradeAssignment(@RequestBody(required = true) int id, double grade, String comment) {
+		Assignment assignment = si.getAssigmentById(id);
+		return si.gradeAssignment(a, grade);
+	}
+
+	@DeleteMapping(value = "delete/{id}", produces = "application/json")
+	public boolean deleteEmployee(@PathVariable("id") int id) {
+		Assignment assignment = as.getAssigmentById(id);
+		return as.deleteAssignment(assignment);
+	}
+}
